ed44e397b7b5cbd43f5c1ba74709e27b6307dc7f
99002762 <72461476+99002762@users.noreply.github.com>
2020-10-09 10:49:39 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -2,7 +2,7 @@
 #define __FUNCTION_H__
 
 #include <stdio.h>
-
+#include<math.h>
 double perimeter_of_triangle_func(double, double, double);
 double area_of_triangle_func(double, double, double);
 int TypeOfTriangle_func(double, double, double);
